1ca1d9c12062a6c73965d8bf063b67fd8ff708d0
Zinaida Prishutova <belyaevazina94@gmail.com>
2025-07-27 08:48:28 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int ticket = 13_676;
-            int forMile = 20;
+        int forMile = 20;
 
-            int bonus = (ticket/forMile);
+        int bonus = (ticket / forMile);
 
-            System.out.println(bonus);
-        }
+        System.out.println(bonus);
     }
+}
