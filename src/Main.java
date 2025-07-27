593cd8bb7a81f3bbddb6de253841132e209b0ffe
Zinaida Prishutova <belyaevazina94@gmail.com>
2025-07-27 08:37:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13_676;
+            int forMile = 20;
+
+            int bonus = (ticket/forMile);
+
+            System.out.println(bonus);
+        }
+    }
