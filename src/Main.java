8477f8fabf5f1ca1cb135d7c181ca714c99ee496
Zinaida Prishutova <belyaevazina94@gmail.com>
2025-07-27 15:03:40 +0300

    sout changed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int ticket = 13_676;
-        int forMile = 20;
+        int forMiles = 20;
 
-        int bonus = (ticket / forMile);
+        int bonus = (ticket / forMiles);
 
-        System.out.println(bonus);
+        System.out.println("Начислено " + bonus + " бонусных миль");
     }
 }
